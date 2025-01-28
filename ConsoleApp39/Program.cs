1eef9d56bd970cf4388a4ba51f83e3b7df566463
Riyy37 <domabruho123@gmail.com>
2025-01-28 16:47:24 +0200

    Task2.1

---
+++ ConsoleApp39/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp39
+{
+    /// <summary>
+    /// Задача 1. Вывести первые пять членов арифметической прогрессии (с использованием цикла), 
+    /// если a1=1, d=4 (например, 1  5  9  13  17).
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int d = 4;
+            int count = 1;
+            
+            while(count <= 5)
+            {
+                Console.WriteLine(a);
+                
+                a += d; //a=a+4
+                count++;
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
